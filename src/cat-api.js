af875ce8ba27c242a54f1ccf3684e27ae92f2fa4
havr1985 <gavrtel1985@gmail.com>
2023-08-11 21:11:48 +0300

    added libruary

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_pBq8QFvm4u2hboEfZvmFHIh1ViUDt30TWnRgJArFkumfFPowkaOpVm41r6rZT8GG";
 
 
-
 export function fetchBreeds() {
  return axios.get('https://api.thecatapi.com/v1/breeds')
      .then((response) => {
@@ -21,4 +20,5 @@ export function fetchCatByBreed(breedId) {
          }
                 return response.data;
             })
-    };
+};
+    
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { fetchBreeds } from "./cat-api";
 import { fetchCatByBreed } from "./cat-api";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import SlimSelect from 'slim-select';
+
 
 const container = document.querySelector('.cat-info');
 const select = document.querySelector('.breed-select');
@@ -14,9 +17,11 @@ errorLoad.classList.add('is-hidden');
 fetchBreeds().then (function(data) {
     const markUp = data.map(({ name, id }) => `<option value="${id}">${name}</option>`).join();
     select.insertAdjacentHTML('beforeend', markUp);
+    
 })
     .catch(function (error) {
-      errorLoad.classList.remove('is-hidden');  
+        errorLoad.classList.remove('is-hidden');
+        Report.failure('Error!', 'Try reloading the page!');  
 })
     .finally(() => loader.classList.add('is-hidden'))
 
@@ -27,19 +32,20 @@ function onChange(event) {
 
     fetchCatByBreed(id)
         .then(function (data) {
-    console.log(data)
             let catName = data[0].breeds[0].name;
             let catImage = data[0].url;
             let catDescription = data[0].breeds[0].description;
             let catTemperament = data[0].breeds[0].temperament;
 
-            container.innerHTML = (`<img src="${catImage}" alt="${catName} width = "600" height = "400">
+            container.innerHTML = (`<img src="${catImage}" alt="${catName} width="600" height = "500">
     <div><h2 class="title">${catName}</h2>
     <p class="description">${catDescription}</p>
     <p class="temperament">TEMPERAMENT: ${catTemperament}</p></div>`);
         })
         .catch(function (error) {
-      errorLoad.classList.remove('is-hidden');  
+            errorLoad.classList.remove('is-hidden');
+            container.innerHTML = '';
+            Report.failure('Error!', 'Try reloading the page!');
         })
     .finally(() => loader.classList.add('is-hidden'))
     
